774a6e308a3e9098deddd0053ccfb47be78b4711
Jon Moroney <darakian@gmail.com>
2018-08-16 14:05:26 -1000

    Lifetime issue, but input syntax works

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,26 @@ use std::sync::Arc;
 //Extended from the examples at https://github.com/hyperium/hyper/tree/master/examples
 type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
+fn test(req: Request<Body>, forward_table: Arc<HashMap<(String, String), hyper::Uri>>) -> Response<Body> {
+    Response::new(Body::from(format!("Request ")))
+}
 
 fn main() {
     let addr = ([127, 0, 0, 1], 3000).into();
     let forward_table: HashMap<(String, String), hyper::Uri> = HashMap::new();
     let arc_table = Arc::new(forward_table);
-
-    let service_forward = move || {
-        let arc_table = arc_table.clone();
-        service_fn_ok(move |req: Request<Body>| {
-            let handler = arc_table.entry((req.method().to_string(), req.uri().path().to_string()));
-            Response::new(Body::from(format!("Request ")))
-        })
-    };
+    //
+    // let service_forward = move || {
+    //     let arc_table = arc_table.clone();
+    //     service_fn_ok(move |req: Request<Body>| {
+    //         let handler = arc_table.entry((req.method().to_string(), req.uri().path().to_string()));
+    //         Response::new(Body::from(format!("Request ")))
+    //     })
+    // };
 
     let server = Server::bind(&addr)
-        .serve(|| service_fn(echo))
+        //.serve(|| service_fn(service_forward))
+        .serve(|| service_fn_ok(|req| test(req, arc_table.clone())))
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening on http://{}", addr);
